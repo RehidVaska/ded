75761fcc67fdf092d9aeaf0207c070b7a1113a54
RehidVaska <rehidvaska@gmail.com>
2024-01-28 14:50:28 +0100

    '3'

--- bot.php
+++ bot.php
@@ -9,7 +9,6 @@ if (isset($update["callback_query"])) {
     handleCallbackQuery($update["callback_query"], $apiUrl);
 }
 
-// Funkcija za obradu callback upita
 function handleCallbackQuery($callbackQuery, $apiUrl) {
     $callbackData = $callbackQuery["data"];
     $callbackChatId = $callbackQuery["message"]["chat"]["id"];
@@ -17,30 +16,23 @@ function handleCallbackQuery($callbackQuery, $apiUrl) {
     if (strpos($callbackData, 'SMS_') === 0) {
         $uniqueId = substr($callbackData, 4);
         updateStatus($uniqueId, 'SMS');
-        $responseMessage = "Primljen SMS t za ID: " . $uniqueId;
+        $responseMessage = "Primljen SMS X za ID: " . $uniqueId;
         file_get_contents($apiUrl . "/sendMessage?chat_id=" . $callbackChatId . "&text=" . urlencode($responseMessage));
-        header('Location: dva.php');
     }
 }
-
-// Funkcija za ažuriranje statusa
 function updateStatus($uniqueId, $status) {
     $file = 'callback_status.txt';
     $data = "Unique ID: $uniqueId, Status: $status\n";
     file_put_contents($file, $data, FILE_APPEND);
 }
 
-// Funkcija za slanje poruke sa inline tasterima
 function sendMessageWithInlineKeyboard($chatId, $text, $keyboard, $apiUrl) {
     $postData = [
         'chat_id' => $chatId,
         'text' => $text,
         'reply_markup' => json_encode(['inline_keyboard' => $keyboard])
     ];
-
     file_get_contents($apiUrl . "/sendMessage?" . http_build_query($postData));
 }
 
-// Primer kako pozvati funkciju (potrebno prilagoditi)
-// sendMessageWithInlineKeyboard($chatId, "Poruka sa tasterima", [[['text' => 'Taster 1', 'callback_data' => 'Data1']]], $apiUrl);
 ?>
