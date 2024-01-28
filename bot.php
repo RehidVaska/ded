8f3dc3254ccc6d066549eaace18337d37a658673
RehidVaska <rehidvaska@gmail.com>
2024-01-28 16:42:46 +0100

    '3'

--- bot.php
+++ bot.php
@@ -8,7 +8,6 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-
 $telegramToken = $_ENV['TELEGRAM_BOT_TOKEN'];
 $telegram = new Api($telegramToken);
 $telegram->addCommand(\Telegram\Bot\Commands\StartCommand::class);
@@ -16,6 +15,12 @@ $telegram->commandsHandler(true);
 $telegram->run();
 
 $update = json_decode(file_get_contents('php://input'), true);
+
+// Dodajte sledeće linije za logovanje grešaka
+$logfile = 'bot_errors.log';
+ini_set('log_errors', 1);
+ini_set('error_log', $logfile);
+error_reporting(E_ALL);
 
 if (isset($update['callback_query'])) {
     $callbackQuery = $update['callback_query'];
